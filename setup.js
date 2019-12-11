8aa72c9a5db4d3d6e129c12ae2c57d221cc99a84
Elias Norrby <elias.norrby@gmail.com>
2019-12-11 10:59:52 +0100

    feat: add setup.js

---
+++ setup.js
@@ -0,0 +1,102 @@
+#!/usr/bin/env node
+const path = require("path");
+const fs = require("fs");
+const chalk = require("chalk");
+const yargs = require("yargs");
+const hasYarn = require("has-yarn")();
+
+const pkgInstall = hasYarn ? "yarn add" : "npm install";
+const pkgInstallDev = `${pkgInstall} -D`;
+
+const peerDeps = "jest @types/jest ts-jest";
+
+yargs
+  .alias("v", "version")
+  .usage("Usage: $0 [options]")
+  .help("h")
+  .alias("h", "help")
+  .option("i", {
+    describe: "Install this package",
+    type: "boolean",
+    alias: "install",
+    default: true,
+  })
+  .describe("no-install", "Skip installing this package")
+  .strict(true);
+
+const argv = yargs.argv;
+
+// Set up logging methods
+const log = {
+  info: msg =>
+    console.log(`${chalk.bgGreen.black(" INFO ")} ${chalk.green(msg)}`),
+  warn: msg =>
+    console.log(`${chalk.bgYellow.black(" WARN ")} ${chalk.yellow(msg)}`),
+  skip: msg => console.log(`${chalk.bgGray(" SKIP ")} ${msg}`),
+  error: msg =>
+    console.log(`${chalk.bgRed.black(" ERROR ")} ${chalk.red(msg)}`),
+};
+
+const packageName = "@eliasnorrby/jest-config";
+
+if (!fs.existsSync("package.json")) {
+  log.error(
+    "No package.json found in the current directory. Make sure you are in the project root. If no package.json exists yet, run `npm init` first.",
+  );
+  process.exit(1);
+}
+
+const jestConfigFile = fs.readFileSync(
+  path.resolve(__dirname, "index.js"),
+  "utf8",
+);
+
+const jestconfig = argv.install
+  ? `\
+module.exports = {
+  ...require("${packageName}"),
+  // Override settings here
+};
+`
+  : `\
+${jestConfigFile}
+`;
+
+// Config files to write
+const CONFIG_FILES = {
+  "jest.config.js": jestconfig,
+};
+
+const failedToWrite = {};
+
+Object.entries(CONFIG_FILES).forEach(([fileName, contents]) => {
+  if (!fs.existsSync(fileName)) {
+    log.info(`Writing ${fileName}`);
+    fs.writeFileSync(fileName, contents, "utf8");
+  } else {
+    log.skip(`${fileName} already exists`);
+    failedToWrite[fileName] = true;
+  }
+});
+
+const pkg = JSON.parse(fs.readFileSync("package.json", "utf8"));
+pkg.scripts = pkg.scripts || {};
+pkg.scripts.test = `jest`;
+log.info("Writing test script to package.json");
+fs.writeFileSync("package.json", JSON.stringify(pkg, null, 2));
+
+log.info(`Installing peer dependencies (${peerDeps})`);
+require("child_process").execSync(`${pkgInstallDev} ${peerDeps}`, {
+  stdio: "inherit",
+});
+
+if (argv.install) {
+  log.info(`Installing self (${packageName})`);
+  require("child_process").execSync(`${pkgInstallDev} ${packageName}`, {
+    stdio: "inherit",
+  });
+} else {
+  log.skip("Skipping install of self");
+}
+
+log.info("Done!");
